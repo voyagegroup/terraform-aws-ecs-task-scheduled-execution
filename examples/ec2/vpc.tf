a1c9ab865ea97c7c45191ec0a02e4f6f48283db0
pei0804 <peeeei0804@gmail.com>
2020-07-16 16:55:46 +0900

    support to launch type of ec2...etc

---
+++ examples/ec2/vpc.tf
@@ -0,0 +1,28 @@
+module vpc {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.44.0"
+
+  name            = var.name
+  cidr            = "10.0.0.0/16"
+  azs             = var.vpc_azs
+  public_subnets  = ["10.0.101.0/24"]
+  private_subnets = ["10.0.104.0/24"]
+
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource aws_security_group this {
+  name   = var.name
+  vpc_id = module.vpc.vpc_id
+}
+
+resource aws_security_group_rule outbount {
+  security_group_id = aws_security_group.this.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
